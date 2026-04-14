d707f6cd541e9c1d89d55dea921fea37e906abc0
BinA <defendersunbin98@naver.com>
2026-04-14 23:30:34 +0900

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -48,7 +48,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = flutter.versionCode
+        versionCode = 3
         versionName = flutter.versionName
     }
 
